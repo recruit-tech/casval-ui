42f7695c02e31942517858770ded77dda390cd37
nishimunea <nishimunea@gmail.com>
2018-07-22 17:29:02 +0900

    Add entrance page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,99 @@
 <template>
-  <div id="app">
-    Hello
+  <div id="app" class="h-100">
+    <entrance :status="status"></entrance>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Entrance from './components/Entrance.vue';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      audit: null,
+      status: 'loading',
+      token: null,
+    };
+  },
+  components: {
+    Entrance,
+  },
+  computed: {
+    auditApiClient: function createAuditApiClient() {
+      return axios.create({
+        baseURL: process.env.VUE_APP_AUDIT_API_ENDPOINT,
+        timeout: process.env.VUE_APP_API_TIMEOUT,
+        headers: { Authorization: `Bearer ${this.token}` },
+        validateStatus: () => true,
+      });
+    },
+  },
+  methods: {
+    generateToken: async function generateToken(auditId, password) {
+      this.status = 'loading';
+      try {
+        const res = await this.auditApiClient.post(`${auditId}/tokens`, { password });
+        switch (res.status) {
+          case 201:
+            this.token = res.data.token;
+            this.getAudit(auditId);
+            break;
+          case 401:
+            this.status = 'restricted-by-password';
+            break;
+          case 403:
+            this.status = 'restricted-by-ip';
+            break;
+          case 404:
+            this.status = 'not-found';
+            break;
+          default:
+            this.status = 'unknown-error';
+        }
+      } catch (e) {
+        this.status = 'unknown-error';
+      }
+    },
+    getAudit: async function getAudit(auditId) {
+      try {
+        const res = await this.auditApiClient.get(auditId);
+        switch (res.status) {
+          case 200:
+          case 304:
+            this.status = 'loaded';
+            this.audit = res.data;
+            window.document.title = `${process.env.VUE_APP_TITLE} - ${this.audit.name}`;
+            break;
+          case 401:
+            this.status = 'invalid-token';
+            break;
+          case 404:
+            this.status = 'not-found';
+            break;
+          default:
+            this.status = 'unknown-error';
+        }
+      } catch (e) {
+        this.status = 'unknown-error';
+      }
+    },
+  },
+  created: function created() {
+    window.eventBus.$on('INITIALIZE_TOKEN', (password) => {
+      const auditId = window.location.hash.substring(2) || '';
+      this.generateToken(auditId, password);
+    });
+  },
+};
 </script>
 
 <style>
+html {
+  height: 100%;
+}
+body {
+  height:100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
+import VueI18n from 'vue-i18n';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import fontawesome from '@fortawesome/fontawesome';
 import faBan from '@fortawesome/fontawesome-free-solid/faBan';
 import faBars from '@fortawesome/fontawesome-free-solid/faBars';
@@ -22,10 +24,23 @@ import faQuestionCircle from '@fortawesome/fontawesome-free-solid/faQuestionCirc
 import faSpinner from '@fortawesome/fontawesome-free-solid/faSpinner';
 import faTimes from '@fortawesome/fontawesome-free-solid/faTimes';
 
-import './assets/css/app.css';
-
 import App from './App.vue';
 
+const en = require('./i18n/en.json');
+const ja = require('./i18n/ja.json');
+
+Vue.use(VueI18n);
+const i18n = new VueI18n({
+  locale: (navigator.browserLanguage || navigator.language || navigator.userLanguage).substr(0, 2),
+  messages: {
+    en,
+    ja,
+  },
+});
+
+Vue.config.productionTip = false;
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 fontawesome.library.add(
   faBan, faBars, faCalendar, faCalendarMinus, faCalendarTimes,
   faCheck, faCheckCircle, faChevronCircleRight, faClock, faDownload,
@@ -33,10 +48,9 @@ fontawesome.library.add(
   faQuestionCircle, faSpinner, faTimes,
 );
 
-Vue.config.productionTip = false;
-
 window.eventBus = new Vue();
 
 new Vue({
+  i18n,
   render: h => h(App),
 }).$mount('#app');
