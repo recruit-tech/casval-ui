4565b698eb9fba23fd3b19cb70f5f3af7b125929
nishimunea <nishimunea@gmail.com>
2018-07-30 08:45:54 +0900

    Remove unnecessary icon definitions

--- src/main.js
+++ src/main.js
@@ -7,24 +7,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import fontawesome from '@fortawesome/fontawesome';
 import faArrowLeft from '@fortawesome/fontawesome-free-solid/faArrowLeft';
-import faBan from '@fortawesome/fontawesome-free-solid/faBan';
 import faBars from '@fortawesome/fontawesome-free-solid/faBars';
 import faCalendar from '@fortawesome/fontawesome-free-solid/faCalendar';
-import faCalendarMinus from '@fortawesome/fontawesome-free-solid/faCalendarMinus';
 import faCalendarTimes from '@fortawesome/fontawesome-free-solid/faCalendarTimes';
-import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
 import faCheckCircle from '@fortawesome/fontawesome-free-solid/faCheckCircle';
 import faChevronCircleRight from '@fortawesome/fontawesome-free-solid/faChevronCircleRight';
-import faClipboardCheck from '@fortawesome/fontawesome-free-solid/faClipboardCheck';
 import faClock from '@fortawesome/fontawesome-free-solid/faClock';
 import faDownload from '@fortawesome/fontawesome-free-solid/faDownload';
 import faExclamationCircle from '@fortawesome/fontawesome-free-solid/faExclamationCircle';
-import faFileAlt from '@fortawesome/fontawesome-free-solid/faFileAlt';
-import faInfoCircle from '@fortawesome/fontawesome-free-solid/faInfoCircle';
 import faPencilAlt from '@fortawesome/fontawesome-free-solid/faPencilAlt';
-import faQuestionCircle from '@fortawesome/fontawesome-free-solid/faQuestionCircle';
 import faSpinner from '@fortawesome/fontawesome-free-solid/faSpinner';
-import faTimes from '@fortawesome/fontawesome-free-solid/faTimes';
 
 import App from './App.vue';
 
@@ -34,6 +26,7 @@ const ja = require('./i18n/ja.json');
 Vue.use(VueI18n);
 const i18n = new VueI18n({
   locale: (navigator.browserLanguage || navigator.language || navigator.userLanguage).substr(0, 2),
+  fallbackLocale: 'en',
   messages: {
     en,
     ja,
@@ -44,10 +37,8 @@ Vue.config.productionTip = false;
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 fontawesome.library.add(
-  faArrowLeft, faBan, faBars, faCalendar, faCalendarMinus, faCalendarTimes,
-  faCheck, faCheckCircle, faChevronCircleRight, faClipboardCheck, faClock,
-  faDownload, faExclamationCircle, faFileAlt, faInfoCircle, faPencilAlt,
-  faQuestionCircle, faSpinner, faTimes,
+  faArrowLeft, faBars, faCalendar, faCalendarTimes, faCheckCircle, faChevronCircleRight,
+  faClock, faDownload, faExclamationCircle, faPencilAlt, faSpinner,
 );
 
 window.eventBus = new Vue();
